5d873a5f44748de3a7aad664c02bd4f26074699e
Alex Scott <calexscott@gmail.com>
2022-05-26 11:53:31 -0700

    Add exp group endpoint

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import hashlib
 from typing import Optional
 
 from fastapi import FastAPI
@@ -5,10 +6,26 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+def get_hash_modulo(id: str, buckets: int = 2) -> int:
+    """
+    Generates a pseudorandom group assignment using MD5 and a modulus.
+    """
+    digest = hashlib.md5(id.encode('ascii')).digest()
+    int_value = int.from_bytes(digest, byteorder='big')
+    return int_value % buckets
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/exp_group/{id}")
+def get_group(id: str):
+    exp_group = get_hash_modulo(id)
+    return {"exp_group": exp_group}
