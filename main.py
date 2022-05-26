6f90d29286a70d45b8b06f67c7ed556da2fc581a
Alex Scott <calexscott@gmail.com>
2022-05-26 13:20:33 -0700

    Move to poetry and add new endpoints

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ import hashlib
 from typing import Optional
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Exp(BaseModel):
+    num_groups: int | None = 2
+    ids: list[str]
 
 
 def get_hash_modulo(id: str, buckets: int = 2) -> int:
@@ -29,3 +35,48 @@ def read_item(item_id: int, q: Optional[str] = None):
 def get_group(id: str):
     exp_group = get_hash_modulo(id)
     return {"exp_group": exp_group}
+
+
+@app.post("/exp_group_list/")
+async def get_exp_groups(ids: list[str]):
+    """Get experiment groups for a list of strings.
+
+    This can be sent a raw JSON list like:
+    ["hello", "world"]
+    """
+    return {
+        "results": [
+            {
+                "id": id,
+                "group": get_hash_modulo(id)
+            }
+            for id in ids
+        ]
+    }
+
+
+@app.post("/exp_group_list2/")
+async def get_exp_groups2(exp: Exp):
+    """Get experiment groups for a list of strings.
+
+    This should be sent JSON like:
+    {
+        "num_groups": 3,
+        "ids": ["hello", "world"]
+    }
+
+    or if you just want the default of 2 groups:
+
+    {
+        "ids": ["hello", "world"]
+    }
+    """
+    return {
+        "results": [
+            {
+                "id": id,
+                "group": get_hash_modulo(id, exp.num_groups)
+            }
+            for id in exp.ids
+        ]
+    }
